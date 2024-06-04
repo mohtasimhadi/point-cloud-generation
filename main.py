1051b93dd6d1e2051ad07ab6fd1f4fb9b377a644
Mohtasim Hadi Rafi <mohtasimhadi@gmail.com>
2024-06-04 09:26:36 -0500

    camera settings put in another file

--- main.py
+++ main.py
@@ -7,78 +7,7 @@ import cv2
 import depthai as dai
 import open3d as o3d
 
-COLOR = True
-OUT_DIR = "output/real-time/"
-
-lrcheck = True
-extended = False
-subpixel = True
-
-median = dai.StereoDepthProperties.MedianFilter.KERNEL_7x7
-
-print("StereoDepth config options:")
-print("    Left-Right check:  ", lrcheck)
-print("    Extended disparity:", extended)
-print("    Subpixel:          ", subpixel)
-print("    Median filtering:  ", median)
-
-pipeline = dai.Pipeline()
-
-monoLeft = pipeline.create(dai.node.MonoCamera)
-monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
-monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
-
-monoRight = pipeline.create(dai.node.MonoCamera)
-monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
-monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
-
-stereo = pipeline.createStereoDepth()
-stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
-stereo.initialConfig.setMedianFilter(median)
-
-stereo.setLeftRightCheck(lrcheck)
-stereo.setExtendedDisparity(extended)
-stereo.setSubpixel(subpixel)
-monoLeft.out.link(stereo.left)
-monoRight.out.link(stereo.right)
-
-config = stereo.initialConfig.get()
-config.postProcessing.speckleFilter.enable = False
-config.postProcessing.speckleFilter.speckleRange = 50
-config.postProcessing.temporalFilter.enable = True
-config.postProcessing.spatialFilter.enable = True
-config.postProcessing.spatialFilter.holeFillingRadius = 2
-config.postProcessing.spatialFilter.numIterations = 1
-config.postProcessing.thresholdFilter.minRange = 400
-config.postProcessing.thresholdFilter.maxRange = 200000
-config.postProcessing.decimationFilter.decimationFactor = 1
-stereo.initialConfig.set(config)
-
-xout_depth = pipeline.createXLinkOut()
-xout_depth.setStreamName("depth")
-stereo.depth.link(xout_depth.input)
-
-xout_colorize = pipeline.createXLinkOut()
-xout_colorize.setStreamName("colorize")
-xout_rect_left = pipeline.createXLinkOut()
-xout_rect_left.setStreamName("rectified_left")
-xout_rect_right = pipeline.createXLinkOut()
-xout_rect_right.setStreamName("rectified_right")
-
-if COLOR:
-    camRgb = pipeline.create(dai.node.ColorCamera)
-    camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
-    camRgb.setIspScale(1, 3)
-    camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
-    camRgb.initialControl.setManualFocus(130)
-    stereo.setDepthAlign(dai.CameraBoardSocket.RGB)
-    camRgb.isp.link(xout_colorize.input)
-else:
-    stereo.rectifiedRight.link(xout_colorize.input)
-
-stereo.rectifiedLeft.link(xout_rect_left.input)
-stereo.rectifiedRight.link(xout_rect_right.input)
-
+from settings import *
 
 class HostSync:
     def __init__(self):
